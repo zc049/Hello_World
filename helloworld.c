fe414e4003091eb88f0a57971ee7cc0d6bb9838a
ZC <zhangcheng049@outlook.com>
2023-03-28 17:24:28 +0800

    pointeruse

--- helloworld.c
+++ helloworld.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 
-int sum(int a, int b)
+int add(int a, int b)
 {
     return a + b;
 }
 
-int use(int *sum(int, int), int a, int b)
+int sub(int a, int b)
 {
-    return sum(a, b);
+    return a - b;
+}
+
+int func(int (*pF)(int, int), int a, int b)
+{
+    return pF(a, b);
 }
 
 int main()
 {
-    printf("%d\n", use(sum, 10, 15));
+    printf("%d\n", func(add, 10, 15));
+    printf("%d\n", func(sub, 15, 10));
     printf("Hello World!");
     return 0;
 }

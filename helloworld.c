3ddc42250e785000c2e0ef60bcf251cc5b763a3d
ZC <zhangcheng049@outlook.com>
2023-03-28 17:14:04 +0800

    pointerFunctionUse

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+int use(int *sum(int, int), int a, int b)
+{
+    return sum(a, b);
+}
+
 int main()
 {
+    printf("%d\n", use(sum, 10, 15));
     printf("Hello World!");
+    return 0;
 }
